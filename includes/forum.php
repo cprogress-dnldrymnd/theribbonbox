a0acb9f44c17e66002c0532505c75aef4f01ff30
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-04-08 14:50:13 +0800

    Update forum.php

--- includes/forum.php
+++ includes/forum.php
@@ -994,7 +994,9 @@ function forum_sidebar()
         $title = 'Related Topics';
         $topics = get_related_topics();
     }
-
+    echo "<pre>";
+    var_dump(get_post_meta(get_the_ID()));
+    echo "</pre>";
 ?>
     <div class="community-posts">
         <div class="featured-box">
