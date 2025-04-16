7568d9d469d3cbf900cb6a8c3b794cd70905b0d3
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-04-16 15:48:39 +0800

    Update header.php

--- header.php
+++ header.php
@@ -98,7 +98,7 @@ if (is_page(41256)) {
                     <?php wp_nav_menu(
                         array(
                             'theme_location' => 'header-menu',
-                            'walker' => new My_Submenu_Wrapper_Walker()
+                            'walker' => new Walker_Nav_Pointers()
                         )
                     ); ?>
                     <div class="navigation-footer-sec">
