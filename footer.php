a9196afa5c03e3b9fbb558e4718ea872c1cc257f
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-04-16 22:29:34 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -50,7 +50,7 @@ $subscribe_popup_bg_colour = get_field('subscribe_popup_bg_colour', $theme_optio
 
       </div>
       <hr>
-      <div style="color: <?= $subscribe_popup_description_colour ?> !important">
+      <div id="subscribe-outer-desc" style="color: <?= $subscribe_popup_description_colour ?> !important">
         <?= wpautop($subscribe_popup_description) ?>
       </div>
       <div class="sub---form" style="color: <?= $subscribe_popup_form_colour ?> !important">
