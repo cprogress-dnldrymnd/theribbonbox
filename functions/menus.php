4df4bb0dcc2ed4dfdf570975b9b555b442593b77
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-04-16 16:36:07 +0800

    Update menus.php

--- functions/menus.php
+++ functions/menus.php
@@ -52,7 +52,7 @@ function trb_av_menu_link_attributes( $atts, $item, $args ) {
     $categories=get_categories($cat_args);
 
     $atts['pageId'] = $item->object_id;
-    $atts['pageIdx'] = $item->parent;
+    $atts['pageIdx'] = $item->object_id;
     //$atts['categoryId'] = $categories[0]->term_id;
 
 //    if (! isset($categories[0])) {
