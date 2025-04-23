e2692107974bff78d097ea02966c0dd684b03d22
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-04-23 17:22:07 +0800

    Update forum.php

--- includes/forum.php
+++ includes/forum.php
@@ -361,7 +361,23 @@ function blog_box($atts)
         'field'     => 'Name',
         'user_id'   => $author
     ));
+    $topic_id = get_the_ID(); // Get the current topic ID within the loop, or specify a topic ID.
 
+    if ( bbp_is_topic( $topic_id ) ) {
+        $forum_id = bbp_get_topic_forum_id( $topic_id );
+    
+        if ( ! empty( $forum_id ) ) {
+            $forum_title = bbp_get_forum_title( $forum_id );
+            $forum_permalink = bbp_get_forum_permalink( $forum_id );
+    
+            echo 'This topic belongs to the forum: <a href="' . esc_url( $forum_permalink ) . '">' . esc_html( $forum_title ) . '</a>';
+        } else {
+            echo 'This topic does not seem to belong to a valid forum.';
+        }
+    } else {
+        echo 'This is not a bbPress topic.';
+    }
+    
 ?>
     <div class="blogs-box d-flex post-box" post-id="<?= $id ?>">
         <?php if (get_the_post_thumbnail_url($id, 'large')) { ?>
