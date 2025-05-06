18dc389963130f171f3f0219a5aa7b7ccd6b825b
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-06 17:45:00 +0800

    cs

--- page-template-community.php
+++ page-template-community.php
@@ -13,26 +13,28 @@ $hero_image = wp_get_attachment_image_url($hero_image, 'full');
 
 <section class="forum-welcome py-5 mt-3 mt-lg-5" style="background-image: url(<?= $hero_image ?>);">
     <div class="container text-center">
-        <h1 class="mb-4"><?php the_title() ?></h1>
-        <?php if (get_the_content()) { ?>
-            <div class="welcome-text w-100">
-                <?php the_content() ?>
-            </div>
-        <?php } ?>
-        <?php if (!is_user_logged_in()) { ?>
+        <div class="inner">
+            <h1 class="mb-4"><?php the_title() ?></h1>
+            <?php if (get_the_content()) { ?>
+                <div class="welcome-text w-100">
+                    <?php the_content() ?>
+                </div>
+            <?php } ?>
+            <?php if (!is_user_logged_in()) { ?>
 
-            <div class="button-box button-box-v2 button-accent mb-3 mt-3">
-                <a href="<?= get_the_permalink(39747) ?>">
-                    LOGIN / CREATE AN ACCOUNT
-                </a>
-            </div>
-        <?php } else { ?>
-            <div class="button-box button-box-v2 button-accent mb-3 mt-3">
-                <a href="/forums">
-                    Write Topic
-                </a>
-            </div>
-        <?php } ?>
+                <div class="button-box button-box-v2 button-accent mb-3 mt-3">
+                    <a href="<?= get_the_permalink(39747) ?>">
+                        LOGIN / CREATE AN ACCOUNT
+                    </a>
+                </div>
+            <?php } else { ?>
+                <div class="button-box button-box-v2 button-accent mb-3 mt-3">
+                    <a href="/forums">
+                        Write Topic
+                    </a>
+                </div>
+            <?php } ?>
+        </div>
     </div>
 </section>
 <section class="forum-latest lg-padding">
