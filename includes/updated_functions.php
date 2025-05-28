a12eb6094a80b4e89774707d119cb8d2411d5de2
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-05-28 12:12:24 +0800

    cs

--- includes/updated_functions.php
+++ includes/updated_functions.php
@@ -262,7 +262,7 @@ function is_past_date($date_string)
 function get_socials_v2()
 {
     ob_start();
-    global $theme_option_page;
+    global $theme_option_page, $theme_icons;
     $facebook_social = get_field('facebook_social', $theme_option_page);
     $pinterest_social = get_field('pinterest_social', $theme_option_page);
     $linkedin_social = get_field('linkedin_social', $theme_option_page);
@@ -274,45 +274,35 @@ function get_socials_v2()
         <?php if ($facebook_social) { ?>
             <div class="col-auto">
                 <a target="_blank" href="<?= $facebook_social ?>">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
-                        <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951" />
-                    </svg>
+                    <?= $theme_icons['facebook'] ?>
                 </a>
             </div>
         <?php } ?>
         <?php if ($pinterest_social) { ?>
             <div class="col-auto">
                 <a target="_blank" href="<?= $pinterest_social ?>">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pinterest" viewBox="0 0 16 16">
-                        <path d="M8 0a8 8 0 0 0-2.915 15.452c-.07-.633-.134-1.606.027-2.297.146-.625.938-3.977.938-3.977s-.239-.479-.239-1.187c0-1.113.645-1.943 1.448-1.943.682 0 1.012.512 1.012 1.127 0 .686-.437 1.712-.663 2.663-.188.796.4 1.446 1.185 1.446 1.422 0 2.515-1.5 2.515-3.664 0-1.915-1.377-3.254-3.342-3.254-2.276 0-3.612 1.707-3.612 3.471 0 .688.265 1.425.595 1.826a.24.24 0 0 1 .056.23c-.061.252-.196.796-.222.907-.035.146-.116.177-.268.107-1-.465-1.624-1.926-1.624-3.1 0-2.523 1.834-4.84 5.286-4.84 2.775 0 4.932 1.977 4.932 4.62 0 2.757-1.739 4.976-4.151 4.976-.811 0-1.573-.421-1.834-.919l-.498 1.902c-.181.695-.669 1.566-.995 2.097A8 8 0 1 0 8 0" />
-                    </svg>
+                    <?= $theme_icons['pinterest'] ?>
                 </a>
             </div>
         <?php } ?>
         <?php if ($linkedin_social) { ?>
             <div class="col-auto">
                 <a target="_blank" href="<?= $linkedin_social ?>">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
-                        <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
-                    </svg>
+                    <?= $theme_icons['linkedin'] ?>
                 </a>
             </div>
         <?php } ?>
         <?php if ($x_social) { ?>
             <div class="col-auto">
                 <a target="_blank" href="<?= $x_social ?>">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-twitter-x" viewBox="0 0 16 16">
-                        <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z" />
-                    </svg>
+                    <?= $theme_icons['x'] ?>
                 </a>
             </div>
         <?php } ?>
         <?php if ($youtube_social) { ?>
             <div class="col-auto">
                 <a target="_blank" href="<?= $youtube_social ?>">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-youtube" viewBox="0 0 16 16">
-                        <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z" />
-                    </svg>
+                     <?= $theme_icons['youtube'] ?>
                 </a>
             </div>
         <?php } ?>
@@ -365,7 +355,7 @@ function create_item_socials_v3($url, $title)
 
         </a>
         <a href="https://x.com/intent/tweet?url=<?= $url  ?>&text=' . $title . '" target="_blank" class="twitter" rel="nofollow">
-            
+
         </a>
     </div>
 <?php
