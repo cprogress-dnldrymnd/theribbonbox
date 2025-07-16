fceba884f5a3e276ab26f4de0033ca145637e691
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-07-16 22:28:36 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -40,7 +40,7 @@ function author_bio()
 
                     <?php if ($article_author_linkedin_url) { ?>
                         <div class="article-author-socials">
-                            <a href="<?= $article_author_linkedin_url ?>" target="_blank"> Connect with Emma:
+                            <a href="<?= $article_author_linkedin_url ?>" target="_blank"> Connect with <?= $article_author ?>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
                                     <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
                                 </svg>
@@ -128,25 +128,22 @@ function article_medically_reviewed_by()
                         <?= wp_get_attachment_image($image_id, 'large') ?>
                     </div>
                     <div class="author-details">
-
                         <div class="article-author">
                             <span><span class="text-label">Medically reviewed by</span> <span class="text-highlight"><?= $reviewed_by ?></span></span>
-                            <?php if ($reviewed_by_url && $link_type == 'linkedin') { ?>
-                                <div class="article-author-socials">
-                                    <a href="<?= $reviewed_by_url ?>" target="_blank">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
-                                            <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
-                                        </svg>
-                                    </a>
-                                </div>
-                            <?php } else if ($link_type == 'external' && $link_text) {  ?>
-                                <a href="<?= $reviewed_by_url ?>" target="_blank">
-                                    <?= $link_text ?>
-                                </a>
-                            <?php } ?>
-
                         </div>
-
+                        <?php if ($reviewed_by_url && $link_type == 'linkedin') { ?>
+                            <div class="article-author-socials">
+                                <a href="<?= $reviewed_by_url ?>" target="_blank">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                                        <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
+                                    </svg>
+                                </a>
+                            </div>
+                        <?php } else if ($link_type == 'external' && $link_text) {  ?>
+                            <a href="<?= $reviewed_by_url ?>" target="_blank">
+                                <?= $link_text ?>
+                            </a>
+                        <?php } ?>
                     </div>
                 </div>
 
