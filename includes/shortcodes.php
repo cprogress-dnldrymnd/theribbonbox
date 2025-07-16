5565210d96cdea4b93491ec60463eabe1556f6bd
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-07-16 22:35:44 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -40,7 +40,7 @@ function author_bio()
 
                     <?php if ($article_author_linkedin_url) { ?>
                         <div class="article-author-socials">
-                            <a href="<?= $article_author_linkedin_url ?>" target="_blank"> Connect with <?= $article_author ?>
+                            <a href="<?= $article_author_linkedin_url ?>" target="_blank"> Connect
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
                                     <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
                                 </svg>
@@ -113,6 +113,7 @@ function article_medically_reviewed_by()
         $reviewed_by_url = get_field("reviewed_by_url", get_the_ID());
         $reviewed_by_photo = get_field("reviewed_by_photo", get_the_ID());
         $link_text = get_field("link_text", get_the_ID());
+        $reviewd_by_text = get_field("reviewd_by_text", get_the_ID());
 
         $placeholder_id = 39014;
         if ($reviewed_by_photo) {
@@ -131,9 +132,14 @@ function article_medically_reviewed_by()
                         <div class="article-author">
                             <span><span class="text-label">Medically reviewed by</span> <span class="text-highlight"><?= $reviewed_by ?></span></span>
                         </div>
+                        <?php if ($reviewd_by_text) { ?>
+                            <div class="article-author-bio">
+                                <?= $reviewd_by_text ?>
+                            </div>
+                        <?php } ?>
                         <?php if ($reviewed_by_url && $link_type == 'linkedin') { ?>
                             <div class="article-author-socials">
-                                <a href="<?= $reviewed_by_url ?>" target="_blank"> Connect with <?= $reviewed_by ?>:
+                                <a href="<?= $reviewed_by_url ?>" target="_blank"> Connect:
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
                                         <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z" />
                                     </svg>
