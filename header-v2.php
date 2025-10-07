2f38349ee5463ba37e99cec23056f816be437a89
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-10-07 22:11:37 +0800

    Update header-v2.php

--- header-v2.php
+++ header-v2.php
@@ -514,7 +514,7 @@
                                 <div class="col-auto">
                                     <div class="offcanvas offcanvas-start" tabindex="-1" id="offCanvasMenu" aria-labelledby="offCanvasMenuLabel">
                                         <div class="offcanvas-body p-0">
-                                            <div class="d-block d-lg-none text-end">
+                                            <div class="close--btn d-block d-lg-none text-end">
                                                 <button type="button" data-bs-dismiss="offcanvas" aria-label="Close">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
                                                         <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
@@ -533,7 +533,13 @@
                                     <div class="offcanvas offcanvas-start " tabindex="-1" id="offCanvasSearch" aria-labelledby="offCanvasSearchLabel">
                                         <div class="offcanvas-body p-0">
                                             <div class="search search-v2">
-                                                <button type="button" class="btn-close d-block d-lg-none" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                                <div class="close--btn d-block d-lg-none text-end">
+                                                    <button type="button" data-bs-dismiss="offcanvas" aria-label="Close">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                                                            <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+                                                        </svg>
+                                                    </button>
+                                                </div>
                                                 <h4 class="d-block d-lg-none">Search</h4>
                                                 <?php echo do_shortcode('[ivory-search id="24768" title="Search for a topic"]'); ?>
                                             </div>
