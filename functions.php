2cc9d981b01b227832ea09d3804435ebb2b64bb2
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-10-07 16:31:16 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -675,6 +675,6 @@ function wpdocs_show_product_widget_shortcode_column( $column, $post_id ) {
 add_filter( 'woocommerce_breadcrumb_defaults', 'wcc_change_breadcrumb_delimiter' );
 function wcc_change_breadcrumb_delimiter( $defaults ) {
 	// Change the breadcrumb delimeter from '/' to '>'
-	$defaults['delimiter'] = ' &gt; ';
+	$defaults['delimiter'] = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/> </svg>';
 	return $defaults;
 }
