804cf8282f4e08ea62ef0d9e42df91042cc5a208
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-10-07 15:11:09 +0800

    cs

--- header-v2.php
+++ header-v2.php
@@ -4,13 +4,27 @@
 <?php global $wp; ?>
 
 <body data-path="<?= $wp->request ?>" <?php body_class(); ?>>
-        <!-- Google Tag Manager (noscript) -->
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PJH7VBG"
-                        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-        <!-- End Google Tag Manager (noscript) -->
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PJH7VBG"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <?php
+    global $theme_logo;
+    ?>
+    <div id="fouc">
+        <div class="site-wrap">
+            <main>
+                <header>
+                    <div class="container">
+                        <div class="row justify-content-between align-items-center">
+                            <div class="col-auto">
+                                <a href="#">
+                                    <?= $theme_logo ?>
+                                </a>
+                            </div>
+                            <div class="col-auto">
 
-        <div id="fouc">
-                <div class="site-wrap">
-                        <main>
-
-                        testxsdsdsds
+                            </div>
+                        </div>
+                    </div>
+                </header>
