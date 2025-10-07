b5ed599cedb4766b44a12c63f4bb2cee2b94893c
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-10-07 21:19:48 +0800

    Update header-v2.php

--- header-v2.php
+++ header-v2.php
@@ -429,6 +429,14 @@
                                             </div>
                                         </div>
                                     </div>
+                                    <button type="button" data-bs-toggle="offcanvas" data-bs-target="#offCanvasSearch" aria-controls="offCanvasSearch" class="d-block d-xl-none">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="17.179" height="17.179" viewBox="0 0 17.179 17.179">
+                                            <g id="Icon_feather-search" data-name="Icon feather-search" transform="translate(0.75 0.75)">
+                                                <path id="Path_1" data-name="Path 1" d="M18.161,11.33A6.83,6.83,0,1,1,11.33,4.5,6.83,6.83,0,0,1,18.161,11.33Z" transform="translate(-4.5 -4.5)" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" />
+                                                <path id="Path_2" data-name="Path 2" d="M28.689,28.689l-3.714-3.714" transform="translate(-13.321 -13.321)" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" />
+                                            </g>
+                                        </svg>
+                                    </button>
 
                                 </div>
                                 <div class="col-auto d-none d-lg-block">
