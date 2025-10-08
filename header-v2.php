fdaed15c989bf2a88d90275f65f0af9fd3af918f
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-10-08 18:10:56 +0800

    Update header-v2.php

--- header-v2.php
+++ header-v2.php
@@ -619,13 +619,7 @@
                                 <div class="col-auto">
                                     <div class="offcanvas offcanvas-start" tabindex="-1" id="offCanvasMenu" aria-labelledby="offCanvasMenuLabel">
                                         <div class="offcanvas-body p-0">
-                                            <div class="close--btn d-block d-lg-none text-end">
-                                                <button type="button" data-bs-dismiss="offcanvas" aria-label="Close">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
-                                                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
-                                                    </svg>
-                                                </button>
-                                            </div>
+                                           
                                             <div class="nav-menu">
                                                 <?php
                                                 wp_nav_menu(array('menu' => 'Header V2',));
