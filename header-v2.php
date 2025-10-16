f4a65e72679e48b30aa82043f7be2837fd3d15c2
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-10-16 19:19:23 +0800

    Update header-v2.php

--- header-v2.php
+++ header-v2.php
@@ -527,7 +527,9 @@
         .m-menu {
             display: none !important;
         }
-
+        #fouc {
+            overflow: hidden;
+        }
         /*responsive*/
 
         @media(min-width: 992px) {
@@ -938,7 +940,7 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <button type="button" class="offCanvasSearch-btn d-block d-lg-none" data-bs-toggle="offcanvas" data-bs-target="#offCanvasSearch" aria-controls="offCanvasSearch" class="d-block d-xl-none">
+                                    <button type="button" class="offCanvasSearch-btn d-block d-md-none" data-bs-toggle="offcanvas" data-bs-target="#offCanvasSearch" aria-controls="offCanvasSearch" class="d-block d-xl-none">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="17.179" height="17.179" viewBox="0 0 17.179 17.179">
                                             <g id="Icon_feather-search" data-name="Icon feather-search" transform="translate(0.75 0.75)">
                                                 <path id="Path_1" data-name="Path 1" d="M18.161,11.33A6.83,6.83,0,1,1,11.33,4.5,6.83,6.83,0,0,1,18.161,11.33Z" transform="translate(-4.5 -4.5)" fill="none" stroke="#000" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" />
