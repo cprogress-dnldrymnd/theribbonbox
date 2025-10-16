9d95e250bf46eb4b8399dd5c113c77ae7e03430a
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-10-16 20:50:41 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1089,19 +1089,4 @@
                     }
                     ?>
                 </div>
-                <div class="hero-v2 trb-border-top">
-                    <div class="top  pt-4 trb-bg-lightyellow trb-px">
-                        <div class="container-fluid">
-                            <div class="breadcrumbs-v2">
-                                <?= woocommerce_breadcrumb(); ?>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="bottom trb-px">
-                        <div class="container-fluid">
-                            <h1>Surrogacy</h1>
-                        </div>
-
-                    </div>
-                </div>
-                
+           
