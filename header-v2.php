8d7eab65ca81167770cf9785e1af6941c4fa4489
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-10-16 16:57:24 +0800

    Update header-v2.php

--- header-v2.php
+++ header-v2.php
@@ -62,7 +62,7 @@
             font-weight: 500;
         }
 
-        #offCanvasMenu #menu-mainmenu li.menu-item .sub-menu {
+        #offCanvasMenu #menu-mainmenu li.menu-item .sub-menu:before {
             content: '' !important;
         }
 
