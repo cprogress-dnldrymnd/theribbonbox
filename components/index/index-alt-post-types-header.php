99555a7bab57db79e4933f7b577661bca47b2ced
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-10-27 17:24:45 +0800

    Update index-alt-post-types-header.php

--- components/index/index-alt-post-types-header.php
+++ components/index/index-alt-post-types-header.php
@@ -1,7 +1,7 @@
 <!-- index-alt-post-types-header.php -->
 <?php $raw_page_title = apply_filters( 'the_title', get_the_title( wp_get_post_parent_id( get_the_ID() ) ) ); ?>
 
-<div class="<?php the_title(); ?>-header <?php echo $raw_page_title; ?>-header page-header-outer" style="">
+<div class="<?php the_title(); ?>-header <?php echo $raw_page_title; ?>-header page-header-outer" style=""ssss>
     <div class="header-text">
         <div class="page-title-outer">
             <?php include 'index-page-title.php'; ?>
@@ -11,4 +11,3 @@
 <?php /*<div class="top-breadcrumb">
                 <div class="breadcrumb-inner">breadcrumb</div>
             </div> */?>
-<?php
