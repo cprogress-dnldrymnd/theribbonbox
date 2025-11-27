d657c619b69dc448f334a4bbdfb79d72dad28a57
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-11-27 19:38:11 +0800

    Update tpl-24.php

--- components/posts/tpl-24.php
+++ components/posts/tpl-24.php
@@ -3,7 +3,7 @@
 if (current_user_can('administrator')) {
     echo 'Only visible to admin';
 
-    $cat = get_top_level_term_by_post_id(get_the_ID(), 'category');
+$cat = get_top_level_term_by_post_id($post['ID'], 'category');
 $category_colour = get_field('category_colour', $cat) ? get_field('category_colour', $cat) : '#3B1527';
 $category_text_color = get_field('category_text_color', $cat) ? get_field('category_text_color', $cat) : '#FFDBD1' ;
 ?>
