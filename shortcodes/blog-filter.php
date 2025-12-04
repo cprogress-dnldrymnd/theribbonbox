d24aabb1b009fa8452893cc13464dd0fcee0a166
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2025-12-05 01:37:54 +0800

    Update blog-filter.php

--- shortcodes/blog-filter.php
+++ shortcodes/blog-filter.php
@@ -2237,11 +2237,11 @@ function blog_filter_function($attr) {
                         
                        // $cat = get_top_level_term_by_post_id($post_id, 'category');
                         $cat = $categoryid;
-                        $category_colour = get_field('category_colour', $cat) ? get_field('category_colour', $cat) : '#3B1527';
+                        $category_colour = get_field('category_colour', $cat) ? get_field('category_colour', $cat) : '#3B1527x';
                         $category_text_color = get_field('category_text_color', $cat) ? get_field('category_text_color', $cat) : '#FFDBD1';
                         $exp_count++;
                         $rtn .=   '
-                            <div '.$categoryid.' class="experts-page-cara tpl-2649" style="--bg-color: '.$category_colour.'; --text-color: '.$category_text_color.'">
+                            <div  class="experts-page-cara tpl-2649" style="--bg-color: '.$category_colour.'; --text-color: '.$category_text_color.'">
                                 <!--<h2>'.$exp_count.'</h2>-->
                                 ' . do_shortcode("[expert_list page='1' title='".$globalCategoryName. " Experts" ."' categoryid='".$categoryid."']") . '
                             </div>
