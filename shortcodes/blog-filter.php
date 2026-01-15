274f87600643d8dfaecebd3d36c10fc9b9ddf108
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-15 17:59:05 +0800

    Update blog-filter.php

--- shortcodes/blog-filter.php
+++ shortcodes/blog-filter.php
@@ -1988,7 +1988,7 @@ function blog_filter_function($attr) {
 
                     //$rtn .= "<h2>here...</h2>";
 
-                    if ($in_count % 2 == 3){
+                    if ($in_count % 2 == 0){
                         //html comments remove by dd
                         //echo '<!-- if ($in_count % 2 == 0) ----- if even -->';
 
