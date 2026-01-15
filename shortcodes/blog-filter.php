a738476622183815421b03c2ded4bb8ad3978e07
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-15 23:03:32 +0800

    Update blog-filter.php

--- shortcodes/blog-filter.php
+++ shortcodes/blog-filter.php
@@ -2197,6 +2197,7 @@ function blog_filter_function($attr)
                         if ($st_3) {
 
                             if (current_user_can('administrator')) {
+                                  $rtn .= '<!--- tpl-30 --->';
                                 $rtn .= do_shortcode('[post_box id=' . $post["ID"] . ']');
                             } else {
                                 include get_template_directory() . '/components/posts/tpl-30.php';
@@ -2206,6 +2207,7 @@ function blog_filter_function($attr)
                             if (! isset($attr["post_type"]) || $attr["post_type"] == "expert_profiles") {
                                 //revamp section
                                 if (current_user_can('administrator')) {
+                                    $rtn .= '<!--- tpl-31 --->';
                                     $rtn .= do_shortcode('[post_box id=' . $post["ID"] . ']');
                                 } else {
                                     include get_template_directory() . '/components/posts/tpl-31.php';
