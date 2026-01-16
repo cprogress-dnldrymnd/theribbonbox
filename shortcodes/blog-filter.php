577269649b0afbaf30012dc9035225d146a0f752
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-16 18:59:45 +0800

    Update blog-filter.php

--- shortcodes/blog-filter.php
+++ shortcodes/blog-filter.php
@@ -1802,15 +1802,21 @@ function blog_filter_function($attr)
                         }
                         $styles_str = implode('-', $styles);
 
-                        
-                        $rtn .= do_shortcode('[post_box count=' . $cnt . ' in_count=' . $in_count. '  id=' . $post["ID"] . ']');
+
+                        $rtn .= do_shortcode('[post_box count=' . $cnt . ' in_count=' . $in_count . '  id=' . $post["ID"] . ']');
 
 
 
                         //second set of posts
                         if (! isset($attr["post_type"]) || ($attr["post_type"] != "expert_profiles" && $attr["post_type"] != "videos" && $attr["post_type"] != "videos/podcasts")) {
-                            
-                            if ($in_count == 7) {
+                            if ($in_count == 5) {
+                                // 1. Close the current div if it is open
+                                if ($post_open_div) {
+                                    $rtn .= '</div>';
+                                }
+                                // 2. Open the new div with the specific classes requested
+                                $rtn .= '<div class="blogs-loop-inner trb-row md-padding mw-1500">';
+                            } else if ($in_count == 7) {
                                 if ($post_open_div) {
                                     $rtn .= '</div>';
                                 }
@@ -2015,7 +2021,7 @@ function blog_filter_function($attr)
                     $post_open_div = true;
                 }
                 $rtn .= '<!--- home-video-even  ---> ';
-                $rtn .= do_shortcode('[post_box count=' . $cnt . ' in_count=' . $in_count. '  id=' . $post["ID"] . ' format="video"]');
+                $rtn .= do_shortcode('[post_box count=' . $cnt . ' in_count=' . $in_count . '  id=' . $post["ID"] . ' format="video"]');
             }
 
             if ($format == "normal") {
@@ -2131,12 +2137,12 @@ function blog_filter_function($attr)
             if ($format == "video") {
                 if ($cnt == 1) {
                     $rtn .= '<!--- home-trending-video  ---> ';
-                    $rtn .= do_shortcode('[post_box_trending_video count=' . $cnt . ' in_count=' . $in_count. ' _trending_video id=' . $post["ID"] . ']');
+                    $rtn .= do_shortcode('[post_box_trending_video count=' . $cnt . ' in_count=' . $in_count . ' _trending_video id=' . $post["ID"] . ']');
 
                     $rtn .= '<div class="blogs-loop-inner trb-row md-padding  mw-1500 trb-px">';
                 } else {
                     $rtn .= '<!--- home-small-podcasts  ---> ';
-                    $rtn .= do_shortcode('[post_box count=' . $cnt . ' in_count=' . $in_count. '  id=' . $post["ID"] . ' format="podcast"]');
+                    $rtn .= do_shortcode('[post_box count=' . $cnt . ' in_count=' . $in_count . '  id=' . $post["ID"] . ' format="podcast"]');
                 }
 
                 if ($cnt == $limit) {
