4bf77f27aa4a0494f525a1d27901579fc0716090
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-16 20:52:28 +0800

    Update blog-filter.php

--- shortcodes/blog-filter.php
+++ shortcodes/blog-filter.php
@@ -1828,13 +1828,13 @@ function blog_filter_function($attr)
                                 if ($post_open_div) {
                                     $rtn .= '</div>';
                                 }
-                                
+
                                 $rtn .=   do_shortcode('[blog_filter format="video" limit="4" order="rand" categoryid="' . $categoryid . '"]');
 
                                 if (empty($post_type)) {
                                     $exp_count++;
                                     // $cat = get_top_level_term_by_post_id($post_id, 'category');
-                                  
+
                                     if ($post_open_div) {
                                         $rtn .= '<div class="blogs-loop-inner blogs-loop-inner-v2 trb-row md-padding  mw-1500 trb-px blogs-loop-inner-5">';
                                     }
@@ -1847,7 +1847,20 @@ function blog_filter_function($attr)
                                         $rtn .= '</div>';
                                     }
 
-                                    
+
+                                    $cat = get_term_by('id', $categoryid, 'category');
+                                    $category_colour = get_field('category_colour', $cat) ? get_field('category_colour', $cat) : '#3B1527';
+                                    $category_text_color = get_field('category_text_color', $cat) ? get_field('category_text_color', $cat) : '#FFDBD1';
+                                    $rtn .=   '
+                            <div ' . $categoryid . ' class="experts-page-cara tpl-2649" style="--bg-color: ' . $category_colour . '; --text-color: ' . $category_text_color . '">
+                                <!--<h2>' . $exp_count . '</h2>-->
+                                ' . do_shortcode("[expert_list page='1' title='" . $globalCategoryName . " Experts" . "' categoryid='" . $categoryid . "']") . '
+                            </div>
+                            <link rel="stylesheet" href="/wp-content/themes/lighttheme/stylesheet/slick.css">
+                            <link rel="stylesheet" href="/wp-content/themes/lighttheme/stylesheet/slick-theme.css">
+                            <script src="/wp-content/themes/lighttheme/js/slick.js"></script>';
+
+
                                     $rtn .= '<div class="blogs-loop-watch-listen">';
                                     $rtn .= '<div class="mw-1500 trb-px">';
                                     $rtn .= '<h2 class="hp-h2">Watch &amp; Listen</h2>';
@@ -1861,17 +1874,6 @@ function blog_filter_function($attr)
                                     $rtn .= do_shortcode("[ad_list]");
 
 
-$cat = get_term_by('id', $categoryid, 'category');
-                                $category_colour = get_field('category_colour', $cat) ? get_field('category_colour', $cat) : '#3B1527';
-                                $category_text_color = get_field('category_text_color', $cat) ? get_field('category_text_color', $cat) : '#FFDBD1';
-                                $rtn .=   '
-                            <div ' . $categoryid . ' class="experts-page-cara tpl-2649" style="--bg-color: ' . $category_colour . '; --text-color: ' . $category_text_color . '">
-                                <!--<h2>' . $exp_count . '</h2>-->
-                                ' . do_shortcode("[expert_list page='1' title='" . $globalCategoryName . " Experts" . "' categoryid='" . $categoryid . "']") . '
-                            </div>
-                            <link rel="stylesheet" href="/wp-content/themes/lighttheme/stylesheet/slick.css">
-                            <link rel="stylesheet" href="/wp-content/themes/lighttheme/stylesheet/slick-theme.css">
-                            <script src="/wp-content/themes/lighttheme/js/slick.js"></script>';
 
                                     if ($post_open_div) {
                                         $rtn .= '<div class="blogs-loop-inner trb-row md-padding  mw-1500 trb-px blogs-loop-inner-6 ">';
@@ -1890,10 +1892,10 @@ $cat = get_term_by('id', $categoryid, 'category');
                                         }
                                     }
                                 }
-                                  $cat = get_term_by('id', $categoryid, 'category');
-                                    $category_colour = get_field('category_colour', $cat) ? get_field('category_colour', $cat) : '#3B1527';
-                                    $category_text_color = get_field('category_text_color', $cat) ? get_field('category_text_color', $cat) : '#FFDBD1';
-                                    $rtn .=   '
+                                $cat = get_term_by('id', $categoryid, 'category');
+                                $category_colour = get_field('category_colour', $cat) ? get_field('category_colour', $cat) : '#3B1527';
+                                $category_text_color = get_field('category_text_color', $cat) ? get_field('category_text_color', $cat) : '#FFDBD1';
+                                $rtn .=   '
                             <div ' . $categoryid . ' class="experts-page-cara tpl-2649" style="--bg-color: ' . $category_colour . '; --text-color: ' . $category_text_color . '">
                                 <!--<h2>' . $exp_count . '</h2>-->
                                 ' . do_shortcode("[expert_list page='1' title='" . $globalCategoryName . " Experts" . "' categoryid='" . $categoryid . "']") . '
@@ -1901,7 +1903,6 @@ $cat = get_term_by('id', $categoryid, 'category');
                             <link rel="stylesheet" href="/wp-content/themes/lighttheme/stylesheet/slick.css">
                             <link rel="stylesheet" href="/wp-content/themes/lighttheme/stylesheet/slick-theme.css">
                             <script src="/wp-content/themes/lighttheme/js/slick.js"></script>';
-
                             }
                         } else {
 
