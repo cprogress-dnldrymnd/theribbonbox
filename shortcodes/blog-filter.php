33ebb79497bb1cb6beb9134e6addb00563f74b12
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-16 21:33:17 +0800

    Update blog-filter.php

--- shortcodes/blog-filter.php
+++ shortcodes/blog-filter.php
@@ -1821,7 +1821,7 @@ function blog_filter_function($attr)
                                     $rtn .= '</div>';
                                 }
 
-                                $rtn .= '[giveaways_here]';
+                                $rtn .= '[_giveaway_list]';
                                 $rtn .= '[become_insider]';
                                 $rtn .= '<div class="blogs-loop-inner trb-row md-padding mw-1500 trb-px">';
                             } else if ($in_count == 10) {
