1a16864c2cad2b7373218869d5aa67d122e39eb3
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-01-16 23:41:42 +0800

    Update custom-shortcodes.php

--- functions/custom-shortcodes.php
+++ functions/custom-shortcodes.php
@@ -471,7 +471,7 @@ function display_followus_function()
     //revamp section
     if (current_user_can('administrator')) { ?>
         <div class="post-follow-us-revamp trb-px">
-            <div class="post-follow-us-revamp-outer mw-1400">
+            <div class="post-follow-us-revamp-outer mw-large">
                 <div class="post-follow-us-revamp-inner">
                     <h2>Follow our <i>Socials</i></h2>
                 </div>
