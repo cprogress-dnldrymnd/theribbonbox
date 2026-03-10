c927fb13197c4af9021487cf87e5faf1784500e4
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-03-10 18:23:09 +0800

    Update header-v2.php

--- header-v2.php
+++ header-v2.php
@@ -31,6 +31,11 @@
             background-color: var(--trb-lightyellow);
         }
 
+        .trb-bg-accent-1 {
+            background-color: var(--trb-accent-1);
+
+        }
+
         header.header-v2 {
             position: sticky;
             top: 0;
@@ -122,6 +127,7 @@
             text-decoration: none;
             display: inline-flex;
             align-items: center;
+            gap: 10px;
         }
 
         .button-accent-2.button-accent-2 a:hover {
@@ -1221,6 +1227,18 @@
     <div id="fouc">
         <div class="site-wrap">
             <?= do_shortcode('[ad_list]') ?>
+            <div class="top-bar trb-bg-accent-1">
+                <div class="container">
+                    <a href="#" data-bs-toggle="modal" data-bs-target="#SignUpModal">
+                        <svg xmlns="http://www.w3.org/2000/svg" id="mail" width="16.853" height="12.64" viewBox="0 0 16.853 12.64">
+                            <g id="Icon-Set">
+                                <path id="mail-2" data-name="mail" d="M427.8,269.533a1.036,1.036,0,0,1-.05.3l-4.69-5.044,4.74-3.687v8.427Zm-13.927,1.025,4.753-5.064,1.8,1.361,1.723-1.37,4.831,5.073c-.075.017-13.032.017-13.107,0Zm-.819-1.025v-8.427l4.74,3.687-4.69,5.044a1.036,1.036,0,0,1-.05-.3Zm14.22-9.48-6.847,5.267-6.847-5.267ZM426.746,259h-12.64A2.107,2.107,0,0,0,412,261.107v8.427a2.107,2.107,0,0,0,2.107,2.107h12.64a2.107,2.107,0,0,0,2.107-2.107v-8.427A2.107,2.107,0,0,0,426.746,259Z" transform="translate(-412 -259)" fill-rule="evenodd" />
+                            </g>
+                        </svg>
+                        Subscribe to our weekly newsletters
+                    </a>
+                </div>
+            </div>
             <header class="header-v2" id="header-main-site">
                 <div class="header--inner py-4 trb-px trb-bg-lightyellow">
                     <div class="container-fluid">
