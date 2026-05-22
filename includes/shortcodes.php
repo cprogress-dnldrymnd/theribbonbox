89dc83b2b662840a487408261661a01f36f83b02
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-05-22 20:36:56 +0800

    Update shortcodes.php

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -360,8 +360,8 @@ function product_widget($atts)
             echo '<div class="swiper-pagination"></div>';
         }
         if ($navigation) {
-            echo '<div class="swiper-button-next"></div>';
-            echo '<div class="swiper-button-prev"></div>';
+            echo '<div class="swiper-button-next-trb"><svg xmlns="http://www.w3.org/2000/svg" width="53" height="53" viewBox="0 0 53 53"> <g id="Group_41" data-name="Group 41" transform="translate(-871 -4259)"> <g id="Ellipse_2" data-name="Ellipse 2" transform="translate(871 4259)" fill="none" stroke="currentColor" stroke-width="1"> <circle cx="26.5" cy="26.5" r="26.5" stroke="none"></circle> <circle cx="26.5" cy="26.5" r="26" fill="none"></circle> </g> <path id="Path_28" data-name="Path 28" d="M4756.17,1529.5l12.3,12.3-12.3,12.3" transform="translate(-3862.67 2743.696)" fill="currentColor"></path> </g> </svg></div>';
+            echo '<div class="swiper-button-prev-trb"><svg xmlns="http://www.w3.org/2000/svg" id="Component_3_1" data-name="Component 3 – 1" width="53" height="53" viewBox="0 0 53 53"> <g id="Group_42" data-name="Group 42" transform="translate(924 4312) rotate(180)"> <g id="Ellipse_2" data-name="Ellipse 2" transform="translate(871 4259)" fill="none" stroke="currentColor" stroke-width="1"> <circle cx="26.5" cy="26.5" r="26.5" stroke="none"></circle> <circle cx="26.5" cy="26.5" r="26" fill="none"></circle> </g> <path id="Path_28" data-name="Path 28" d="M4756.17,1529.5l12.3,12.3-12.3,12.3" transform="translate(-3862.67 2743.696)" fill="currentColor"></path> </g> </svg></div>';
         }
 
         echo '</div>'; // Close product-widget--outer
@@ -404,8 +404,8 @@ function product_widget($atts)
                 // Conditionally append DOM targeting for navigation
                 <?php if ($navigation) : ?>
                 swiperOptions.navigation = {
-                    nextEl: "#" + targetId + " .swiper-button-next",
-                    prevEl: "#" + targetId + " .swiper-button-prev",
+                    nextEl: "#" + targetId + " .swiper-button-next-trb",
+                    prevEl: "#" + targetId + " .swiper-button-prev-trb",
                 };
                 <?php endif; ?>
 
