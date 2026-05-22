d701a5159ad3fef4d1b705d520069b5be6c630e0
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-05-22 21:22:46 +0800

    Update two-columns-2.php

--- sections/two-columns-2.php
+++ sections/two-columns-2.php
@@ -37,16 +37,16 @@
             <div class="nav-tabs-holder-inner d-flex align-items-center justify-content-between gap-3">
                 <ul class="nav nav-tabs gap-2 gap-lg-5" id="tabFeatured" role="tablist">
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link active" id="Discounts-tab" data-bs-toggle="tab" data-bs-target="#Discounts" type="button" role="tab" aria-controls="Discounts" aria-selected="true">Discounts</button>
+                        <button class="nav-link trb-petal-color active" id="Discounts-tab" data-bs-toggle="tab" data-bs-target="#Discounts" type="button" role="tab" aria-controls="Discounts" aria-selected="true">Discounts</button>
                     </li>
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="Products-tab" data-bs-toggle="tab" data-bs-target="#Products" type="button" role="tab" aria-controls="Products" aria-selected="false">Products</button>
+                        <button class="nav-link trb-petal-color" id="Products-tab" data-bs-toggle="tab" data-bs-target="#Products" type="button" role="tab" aria-controls="Products" aria-selected="false">Products</button>
                     </li>
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="Giveaways-tab" data-bs-toggle="tab" data-bs-target="#Giveaways" type="button" role="tab" aria-controls="Giveaways" aria-selected="false">Giveaways</button>
+                        <button class="nav-link trb-petal-color" id="Giveaways-tab" data-bs-toggle="tab" data-bs-target="#Giveaways" type="button" role="tab" aria-controls="Giveaways" aria-selected="false">Giveaways</button>
                     </li>
                     <li class="nav-item" role="presentation">
-                        <button class="nav-link" id="Guides-tab" data-bs-toggle="tab" data-bs-target="#Guides" type="button" role="tab" aria-controls="Guides" aria-selected="false">Guides</button>
+                        <button class="nav-link trb-petal-color" id="Guides-tab" data-bs-toggle="tab" data-bs-target="#Guides" type="button" role="tab" aria-controls="Guides" aria-selected="false">Guides</button>
                     </li>
                 </ul>
                 <div class="button-box button-box-v2 button-bordered">
