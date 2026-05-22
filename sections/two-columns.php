15d0a30be6731a8a95c083005b87725c9423fc2c
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-05-22 21:14:43 +0800

    Update two-columns.php

--- sections/two-columns.php
+++ sections/two-columns.php
@@ -2,13 +2,13 @@
     <div class="container">
         <div class="inner">
             <h2 class="trb-wine-color large-heading mb-5">Wellbeing</h2>
-            <div class="row">
-                <div class="col-lg-6">
+            <div class="row gap-3 gap-lg-5">
+                <div class="col-lg-7">
                     <div class="image-box">
                         <img src="https://theribbonbox.com/wp-content/uploads/2026/05/Path-21.png" alt="">
                     </div>
                 </div>
-                <div class="col-lg-6">
+                <div class="col-lg-5">
                     <h3 class="trb-wine-color">Find Balance and Vitality with Trusted Wellbeing Essentials</h3>
                     <div class="desc trb-wine-color">
                         <p>Discover handpicked products, expert guides, and exclusive discounts to support your physical and mental health. Whether you’re looking to enhance energy, manage stress, or promote overall wellness, we’ve got you covered with the best in wellbeing care.</p>
