c396251f0035963223b00da10b7c9e0b8b9d8dc4
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-05-22 21:19:39 +0800

    Update two-columns-2.php

--- sections/two-columns-2.php
+++ sections/two-columns-2.php
@@ -1,3 +1,15 @@
+<style>
+   
+    .product-tabs-fertility:before {
+        content: '';
+        position: absolute;
+        background-color: var(--trb-wine);
+        height: 100px;
+        left: 0;
+        top: 0;
+        right: 0;
+    }
+</style>
 <section class="two-columns bg-wine">
     <div class="container">
         <div class="inner">
@@ -18,3 +30,45 @@
         </div>
     </div>
 </section>
+<div class="product-tabs product-tabs-fertility trb-mt-large trb-mb-medium position-relative">
+    <div class="container position-relative">
+        <div class="nav-tabs-holder">
+            <h3 class="trb-wine-color mb-4"><i>Featured</i> Picks</h3>
+
+            <div class="nav-tabs-holder-inner d-flex align-items-center justify-content-between gap-3">
+                <ul class="nav nav-tabs gap-2 gap-lg-5" id="tabFeatured" role="tablist">
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active" id="Discounts-tab" data-bs-toggle="tab" data-bs-target="#Discounts" type="button" role="tab" aria-controls="Discounts" aria-selected="true">Discounts</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="Products-tab" data-bs-toggle="tab" data-bs-target="#Products" type="button" role="tab" aria-controls="Products" aria-selected="false">Products</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="Giveaways-tab" data-bs-toggle="tab" data-bs-target="#Giveaways" type="button" role="tab" aria-controls="Giveaways" aria-selected="false">Giveaways</button>
+                    </li>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link" id="Guides-tab" data-bs-toggle="tab" data-bs-target="#Guides" type="button" role="tab" aria-controls="Guides" aria-selected="false">Guides</button>
+                    </li>
+                </ul>
+                <div class="button-box button-box-v2 button-bordered">
+                    <a href="#">ALL WELLBEING DISCOUNTS</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="tab-content" id="tabFeaturedContent">
+            <div class="tab-pane fade show active" id="Discounts" role="tabpanel" aria-labelledby="Discounts-tab">
+                <?= do_shortcode('[product_widget id="49836"]') ?>
+            </div>
+            <div class="tab-pane fade" id="Products" role="tabpanel" aria-labelledby="Products-tab">
+                <?= do_shortcode('[product_widget id="49836"]') ?>
+            </div>
+            <div class="tab-pane fade" id="Giveaways" role="tabpanel" aria-labelledby="Giveaways-tab">
+                <?= do_shortcode('[product_widget id="49836"]') ?>
+            </div>
+            <div class="tab-pane fade" id="Guides" role="tabpanel" aria-labelledby="Guides-tab">
+                <?= do_shortcode('[product_widget id="49836"]') ?>
+            </div>
+        </div>
+    </div>
+</div>
