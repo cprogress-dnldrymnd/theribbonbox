08cd039c3f00e0829bc8bd45bda9fcc769b4babf
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-05-22 19:12:49 +0800

    Create category-navigation.php

---
+++ sections/category-navigation.php
@@ -0,0 +1,14 @@
+<section class="category-navigation">
+    <div class="container">
+        <div class="inner">
+            <div class="row">
+                <div class="col">
+                    <a href="">
+                        <span class="cat-decor">JUMP TO</span>
+                        <div class="cat-text">Wellbeing</div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
