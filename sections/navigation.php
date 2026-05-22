34ad4d1ccd6b25cceeba0a2ecacf6a1c7ef92921
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-05-22 18:52:21 +0800

    Update navigation.php

--- sections/navigation.php
+++ sections/navigation.php
@@ -1,14 +1,21 @@
 <section class="trb-picks-nav trb-bg-accent-2">
     <div class="container">
-        <h1 class="trb-picks-nav-title">TRB Picks</h1>
-        <nav class="trb-picks-nav-menu">
-            <ul>
-                <li><a href="#">All</a></li>
-                <li><a href="#">Movies</a></li>
-                <li><a href="#">TV</a></li>
-                <li><a href="#">Books</a></li>
-                <li><a href="#">Music</a></li>
-            </ul>
-        </nav>
+        <div class="row">
+            <div class="col-lg-3">
+                <a class="trb-picks-nav-title">TRB Picks</a>
+            </div>
+            <div class="col-lg-6 text-center">
+                <nav class="trb-picks-nav-menu">
+                    <ul class="d-flex align-items-center m-0">
+                        <li><a href="#">All</a></li>
+                        <li><a href="#">Movies</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">Books</a></li>
+                        <li><a href="#">Music</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-lg-3"></div>
+        </div>
     </div>
 </section>
