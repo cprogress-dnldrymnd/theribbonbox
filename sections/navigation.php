bda8507dda2444aebbbd07f339b84ae008c33dbb
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-05-22 18:59:50 +0800

    Update navigation.php

--- sections/navigation.php
+++ sections/navigation.php
@@ -7,11 +7,10 @@
             <div class="col-lg-6 text-center">
                 <nav class="trb-picks-nav-menu">
                     <ul class="p-0 d-flex align-items-center justify-content-center m-0 gap-3 gap-lg-5">
-                        <li><a class="text-white" href="#">All</a></li>
-                        <li><a class="text-white" href="#">Movies</a></li>
-                        <li><a class="text-white" href="#">TV</a></li>
-                        <li><a class="text-white" href="#">Books</a></li>
-                        <li><a class="text-white" href="#">Music</a></li>
+                        <li><a class="text-white" href="#">SHOP</a></li>
+                        <li><a class="text-white" href="#">DISCOUNTS</a></li>
+                        <li><a class="text-white" href="#">GUIDES</a></li>
+                        <li><a class="text-white" href="#">GIVEAWAYS</a></li>
                     </ul>
                 </nav>
             </div>
