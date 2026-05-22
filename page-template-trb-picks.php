e689e5728d634735d35e34bc2a4fa3e461794420
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-05-22 19:26:36 +0800

    Update page-template-trb-picks.php

--- page-template-trb-picks.php
+++ page-template-trb-picks.php
@@ -11,18 +11,23 @@
         --trb-wine: #3B1527;
         --trb-coral: #F77D67;
     }
+
     .trb-green-color {
         color: var(--trb-green);
     }
+
     .trb-wine-color {
         color: var(--trb-wine);
     }
+
     .trb-coral-color {
         color: var(--trb-coral);
     }
+
     .rounded {
         border-radius: 20px;
     }
+
     .trb-picks-nav {
         padding-block: 1rem;
     }
@@ -42,6 +47,7 @@
     .picks-nav-title {
         color: #FFDBD1;
     }
+
     .page-title {
         padding-top: 3rem;
         padding-bottom: 5rem;
@@ -51,35 +57,53 @@
         font-size: 3rem;
         font-weight: bold;
     }
+
     .page-title .desc {
         font-size: 1.25rem;
         max-width: 700px;
         margin: 0 auto;
     }
+
     .page-title .woocommerce-breadcrumb {
         text-align: center;
         margin-bottom: 1.5rem;
     }
-    .post-main-content p, p.text, .expert-inner p, .category-inner-pop-text p, .desc p, .post-excerpt, .bbp-forum-content, .bbp-reply-content p, .bio p, .activity-item p, .blog-l-text p {
+
+    .post-main-content p,
+    p.text,
+    .expert-inner p,
+    .category-inner-pop-text p,
+    .desc p,
+    .post-excerpt,
+    .bbp-forum-content,
+    .bbp-reply-content p,
+    .bio p,
+    .activity-item p,
+    .blog-l-text p {
         font-size: 1rem !important;
     }
 
     .category-navigation .inner {
-        background-color: #EDF7F6;;
+        background-color: #EDF7F6;
+        ;
         padding-block: clamp(2rem, 6vw, 115px);
     }
+
     .category-navigation-holder {
         padding-inline: 2rem;
         max-width: 1000px;
     }
+
     .cat-nav-holder {
         display: flex;
         flex-direction: column;
     }
+
     .cat-decor {
         font-weight: 600;
         padding-bottom: 1rem;
     }
+
     .cat-text {
         font-family: Playfair Display, serif;
         font-size: 2rem;
@@ -87,7 +111,6 @@
         border-top: 1px solid;
         padding-top: 1rem;
     }
-
 </style>
 <?php get_template_part('sections/navigation') ?>
 <?php get_template_part('sections/page-title') ?>
