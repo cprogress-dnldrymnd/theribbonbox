280950757fb7b56cf10d0eff92ee42fb37453f0b
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-05-22 19:01:21 +0800

    Create page-title.php

---
+++ sections/page-title.php
@@ -0,0 +1,15 @@
+<section class="page-title">
+    <div class="container">
+        <div class="inner">
+            <div class="breadcrumbs-v2">
+                <?= woocommerce_breadcrumb(); ?>
+            </div>
+            <h1>Discover Trusted Products, Exclusive Offers & Expert Picks For Every Stage of Your Journey</h1>
+            <div class="desc">
+                <p>
+                    We've sourced the latest exclusive offers and discount codes across trusted fertility, pregnancy, parenting, and lifestyle brands - helping you save at every stage of your journey.
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
