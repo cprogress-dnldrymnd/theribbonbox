3bc189026986f1dd5f206427103fed0fb70887a0
cprogress-dnldrymnd <129990906+cprogress-dnldrymnd@users.noreply.github.com>
2026-06-12 00:53:02 +0800

    all

--- functions/page-builder.php
+++ functions/page-builder.php
@@ -1,6 +1,6 @@
 <?php
 if ( ! defined( 'TRB_BUILDER_VERSION' ) ) {
-    define( 'TRB_BUILDER_VERSION', '1.1.3' );
+    define( 'TRB_BUILDER_VERSION', '1.1.4' );
 }
 /*-----------------------------------------------------------------------------------*/
 /* TRB Page Builder
