c0b19e78daead229a08f796e9261ca244e11e5f7
cprogress-dnldrymnd <83536441+raymundonald@users.noreply.github.com>
2026-06-12 22:33:42 +0800

    style.css

--- functions/page-builder.php
+++ functions/page-builder.php
@@ -1,6 +1,6 @@
 <?php
 if ( ! defined( 'TRB_BUILDER_VERSION' ) ) {
-    define( 'TRB_BUILDER_VERSION', '1.5.5' );
+    define( 'TRB_BUILDER_VERSION', '1.5.6' );
 }
 /*-----------------------------------------------------------------------------------*/
 /* TRB Page Builder
